f38e7fec0a476578451ce1e2a0bfa788ecafa29c
Ashin <ahmedashin999@gmail.com>
2020-08-08 17:17:55 +0600

    Updates

---
+++ precache-manifest.51f9917327441c775c3d4d767265f1e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21de81c267ee5a22877f8d3f9cede540",
+    "url": "/mini-messenger/index.html"
+  },
+  {
+    "revision": "f802d41d93053ac942c2",
+    "url": "/mini-messenger/static/css/main.9f426bd2.chunk.css"
+  },
+  {
+    "revision": "cdf7769fac5282e339b7",
+    "url": "/mini-messenger/static/js/2.2173fde2.chunk.js"
+  },
+  {
+    "revision": "f802d41d93053ac942c2",
+    "url": "/mini-messenger/static/js/main.c4e187e2.chunk.js"
+  },
+  {
+    "revision": "e5e666f657a7e84d44af",
+    "url": "/mini-messenger/static/js/runtime~main.92340b1b.js"
+  }
+]);
